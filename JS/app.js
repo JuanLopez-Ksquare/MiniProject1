bb1135d080dc7b617cb9b84d7d685aeeb25e4d93
Juan Carlos <102171185+JuanLopez-Ksquare@users.noreply.github.com>
2022-04-03 19:55:42 -0500

    Complete2Story
    Added all the buttons and the clickable functionality

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,59 @@
+let zeroBtn = document.querySelector(".zero");
+let oneBtn = document.querySelector(".one");
+let twoBtn = document.querySelector(".two");
+let threeBtn = document.querySelector(".three");
+let fourBtn = document.querySelector(".four");
+let fiveBtn = document.querySelector(".five");
+let sixBtn = document.querySelector(".six");
+let sevenBtn = document.querySelector(".seven");
+let eightBtn = document.querySelector(".eight");
+let nineBtn = document.querySelector(".nine");
 let equalsBtn = document.querySelector(".equals");
+
+zeroBtn.addEventListener("click", () => {
+    console.log("0");
+})
+
+oneBtn.addEventListener("click", () => {
+    console.log("1");
+})
+
+twoBtn.addEventListener("click", () => {
+    console.log("2");
+})
+
+threeBtn.addEventListener("click", () => {
+    console.log("3");
+})
+
+fourBtn.addEventListener("click", () => {
+    console.log("4");
+})
+
+fiveBtn.addEventListener("click", () => {
+    console.log("5");
+})
+
+sixBtn.addEventListener("click", () => {
+    console.log("6");
+})
+
+sevenBtn.addEventListener("click", () => {
+    console.log("7");
+})
+
+eightBtn.addEventListener("click", () => {
+    console.log("8");
+})
+
+nineBtn.addEventListener("click", () => {
+    console.log("9");
+})
 
 equalsBtn.addEventListener("click", () => {
     console.log("=");
 })
+
+
 
 
